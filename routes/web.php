d1eb09a51df2d2068de9799d9bf83b3eb637db83
huizai <237002893@qq.com>
2020-10-27 11:10:58 +0000

    用户界面 - 订单列表页

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 
     Route::post('orders', 'OrdersController@store')->name('orders.store');
+    Route::get('orders', 'OrdersController@index')->name('orders.index');
 });
 
 // 这个路由和上面 'products/favorites' 路由有冲突，所以放到下面
