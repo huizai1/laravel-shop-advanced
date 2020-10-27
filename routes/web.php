e824b5d88d29576a1c9ebb44a9619f543ac848af
huizai <237002893@qq.com>
2020-10-27 08:40:45 +0000

    创建订单

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('cart', 'CartController@add')->name('cart.add');
     Route::get('cart', 'CartController@index')->name('cart.index');
     Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+
+    Route::post('orders', 'OrdersController@store')->name('orders.store');
 });
 
 // 这个路由和上面 'products/favorites' 路由有冲突，所以放到下面
