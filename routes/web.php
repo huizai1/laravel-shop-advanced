80d17b79bcb8d6ccbae5a1fd50457a116942f57b
huizai <237002893@qq.com>
2020-11-07 10:31:47 +0000

    众筹商品详情页

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('orders/{order}/apply_refund', 'OrdersController@applyRefund')->name('orders.apply_refund');
 
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
+
+    Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
 });
 
 // 这个路由和上面 'products/favorites' 路由有冲突，所以放到下面
