2e9d32d46f7705228baa4cbec9f78a898d963359
huizai <237002893@qq.com>
2020-11-09 11:53:38 +0000

    创建分期付款

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,8 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
 
     Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
+
+    Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
 });
 
 // 这个路由和上面 'products/favorites' 路由有冲突，所以放到下面
