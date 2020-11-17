2b9f9d8dcefbb3fbb69bd65f491dfdb971cf2b39
huizai <237002893@qq.com>
2020-11-17 08:58:28 +0000

    移除无用路由

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+// 可以看到 deploy:writable 这个任务已经成功执行了，但是在执行 artisan:migrate 时报错了，根据报错信息可以知道这是因为 Laravel 不支持对 api/user 这个闭包路由做缓存，这个路由是在创建 Laravel 项目时默认带的，我们在项目中并没有用到，因此可以删除：
+/*
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+*/
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
